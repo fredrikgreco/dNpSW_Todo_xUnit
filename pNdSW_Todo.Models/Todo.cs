b70e72ef457e9ce6dbed073ca488bf620ee19646
fredrikgreco <112250686+fredrikgreco@users.noreply.github.com>
2023-12-23 20:02:03 +0100

    Models here now

---
+++ pNdSW_Todo.Models/Todo.cs
@@ -0,0 +1,11 @@
+﻿namespace Models_Todo
+{
+    public class Todo
+    {
+        public int Id {  get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public bool IsDone { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
